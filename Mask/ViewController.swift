74cf98e6d3c85394137919f79ca926cbcd6fef86
Jack Cook <jack@jackcook.nyc>
2015-03-08 08:47:35 -0400

    Implemented advertisement switch

--- Mask/ViewController.swift
+++ Mask/ViewController.swift
@@ -6,20 +6,23 @@
 //  Copyright (c) 2015 Jack Cook. All rights reserved.
 //
 
+import Alamofire
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var advertisementSwitch: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func switchChanged(sender: AnyObject) {
+        let on = advertisementSwitch.on ? "off" : "on"
+        request(.GET, "http://104.131.190.96:8080/ads\(on)")
     }
-
-
+    
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return .LightContent
+    }
 }
-
